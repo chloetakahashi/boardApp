cc5d4e4992e8328b258ad6f006ca29eb43a9dbe5
redheadchloe <chloe.socrates.7z@gmail.com>
2023-03-26 00:51:27 +0900

    add login and signup functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-   root "static_pages_controller#top"
+  root "static_pages_controller#top"
+
+  # root :to => 'users#index'
+  resources :users, only: %i[new create]
+
+  get 'login' => 'user_sessions#new', :as => :login
+  post 'login' => "user_sessions#create"
+  delete 'logout' => 'user_sessions#destroy', :as => :logout
 end

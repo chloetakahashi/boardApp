1d825ca8e7e483a066c23d4ee7178f46aa5f4c76
redheadchloe <chloe.socrates.7z@gmail.com>
2023-03-27 21:56:09 +0900

    add internalization and bootstrap flash messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,11 +3,12 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  def create
+    def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to login_path
+      redirect_to login_path, success: t('.success')
     else
+      flash.now[:danger] = t('.fail')
       render :new
     end
   end
--- config/application.rb
+++ config/application.rb
@@ -37,5 +37,10 @@ module RunteqNormal
       g.assets          false
       g.skip_routes     true
     end
+
+    # langauges
+    config.i18n.available_locales = %i[ja en]
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
   end
 end

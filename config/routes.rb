88509c1fa9cf46cfde40cff744828f700c5e0cc2
redheadchloe <chloe.socrates.7z@gmail.com>
2023-04-08 18:31:43 +0900

    add edit and delete functionality for board

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   # root :to => 'users#index'
   resources :users, only: %i[new create]
-  resources :boards, only: %i[index new create show] do
+  resources :boards, only: %i[index new create show edit update destroy] do
     resources :comments, only: %i[create], shallow: true
   end
 
